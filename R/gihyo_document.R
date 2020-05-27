2a8d018b4b119bd3afafa271d586b8ecc9fb8a46
Atsushi Yasumoto <atsushi@hacarus.com>
2020-05-27 22:32:39 +0900

    lint

--- R/gihyo_document.R
+++ R/gihyo_document.R
@@ -27,12 +27,11 @@
 #' @param ... Arguments passed to `md_document`
 #' @export
 gihyo_document <- function(
-  md_extensions = NULL,
-  pandoc_args = NULL,
-  html_preview = TRUE,
-  ...
-) {
-  base_format = rmarkdown::github_document(
+                           md_extensions = NULL,
+                           pandoc_args = NULL,
+                           html_preview = TRUE,
+                           ...) {
+  base_format <- rmarkdown::github_document(
     md_extensions = c("+ignore_line_breaks", md_extensions),
     pandoc_args = c(
       "--wrap=none",
@@ -44,39 +43,39 @@ gihyo_document <- function(
   )
 
   # substitute template
-  index_template = which(base_format$pandoc$args == "--template")
-  template = system_file("template", "template.md")
+  index_template <- which(base_format$pandoc$args == "--template")
+  template <- system_file("template", "template.md")
   if (length(index_template) == 1L) {
-    base_format$pandoc$args[index_template + 1L] = template
+    base_format$pandoc$args[index_template + 1L] <- template
   } else {
-    base_format$pandoc$args = c(base_format$pandoc$args, "--template", template)
+    base_format$pandoc$args <-
+      c(base_format$pandoc$args, "--template", template)
   }
 
-  knitr_options = rmarkdown::knitr_options(
+  knitr <- rmarkdown::knitr_options(
     opts_chunk = list(
-      fig.path = 'img/',
+      fig.path = "img/",
       prompt = TRUE,
       collapse = TRUE,
-      comment = ''
+      comment = ""
     ),
     opts_hooks = list(
       fig.cap = function(options) {
-        options$fig.cap = sprintf(
-          "(#fig:%s) %s", options$label, options$fig.cap
-        )
+        options$fig.cap <-
+          sprintf("(#fig:%s) %s", options$label, options$fig.cap)
         options
       }
     )
   )
 
   rmarkdown::output_format(
-    knitr = knitr_options,
+    knitr = knitr,
     pandoc = NULL,
     clean_supporting = FALSE,
     base_format = base_format
   )
 }
 
-system_file = function(..., package = "gihyodown") {
+system_file <- function(..., package = "gihyodown") {
   system.file(..., package = package)
 }
