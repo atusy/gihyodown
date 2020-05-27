33c6789ca6c4c053d672ff9d4823c859bc0d5490
Atsushi Yasumoto <atsushi@hacarus.com>
2020-05-27 22:16:39 +0900

    update documents

--- R/gihyo_document.R
+++ R/gihyo_document.R
@@ -1,10 +1,35 @@
 #' Gihyo Flavored Markdown
-#' @inheritParams rmarkdown::md_document
+#'
+#' This R Markdown format is based on `rmarkdown::github_document`.
+#' Main changes are
+#' (1) usage of custom tempalte,
+#' (2) line breaks are ignored instead of treated as hard line breaks,
+#' (3) support cross reference like bookdown,
+#' (4) echoes and results are styled by REPL-like format,
+#' (5) figures are saved under img directory
+#'
+#' Cross reference can be configured via YAML frontmatter.
+#' For example, if `(#fig:foo)` should be rendered as `Figure 2.1`,
+#' then setup the YAML below. Keys for `labels` can be any.
+#' In other words, users can define original ones.
+#'
+#' ``` yaml
+#' output: gihyodown::gihyo_document
+#' crossref:
+#'   section: 2
+#'   labels:
+#'     fig: "Figure "
+#'     tab: "Table "
+#'     eqn: "Equation "
+#' ```
+#'
+#' @inheritParams rmarkdown::github_document
 #' @param ... Arguments passed to `md_document`
 #' @export
 gihyo_document <- function(
   md_extensions = NULL,
   pandoc_args = NULL,
+  html_preview = TRUE,
   ...
 ) {
   base_format = rmarkdown::github_document(
@@ -14,9 +39,11 @@ gihyo_document <- function(
       "--lua-filter", system_file("lua", "crossref.lua")
     ),
     hard_line_breaks = FALSE,
+    html_preview = TRUE,
     ...
   )
 
+  # substitute template
   index_template = which(base_format$pandoc$args == "--template")
   template = system_file("template", "template.md")
   if (length(index_template) == 1L) {
