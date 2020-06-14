f6deb8903339a22696682cb392724bf595fb903c
Atsushi Yasumoto <atsushi@hacarus.com>
2020-06-14 19:22:28 +0900

    post_processor specifies title to supress warning

--- R/gihyo_document.R
+++ R/gihyo_document.R
@@ -80,7 +80,10 @@ post_processor <- function(gray_preview) {
       output_file,
       output_format = rmarkdown::html_document(
         highlight = if (gray_preview) "monochrome" else "default",
-        css = if (gray_preview) system_file("css", "html-preview.css")
+        css = if (gray_preview) system_file("css", "html-preview.css"),
+        pandoc_args = c(
+          "--metadata", sprintf('title="%s"', metadata$title)
+        )
       ),
       output_file = xfun::with_ext(output_file, "html")
     )
