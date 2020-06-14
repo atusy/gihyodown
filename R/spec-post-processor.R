57b2b91a6b8e85700ebeea558bd362d5142056b3
atusy <30277794+atusy@users.noreply.github.com>
2020-06-15 08:50:49 +0900

    fix ref to footnotes rendered as math in preview

--- R/spec-post-processor.R
+++ R/spec-post-processor.R
@@ -2,15 +2,17 @@ spec_post_processor <- function(gray_preview) {
   force(gray_preview)
 
   function(metadata, input_file, output_file, clean, verbose) {
+    preview_format <- rmarkdown::html_document(
+      highlight = if (gray_preview) "monochrome" else "default",
+      css = if (gray_preview) system_file("css", "html-preview.css"),
+      pandoc_args = c(
+        "--metadata", sprintf('title="%s"', metadata$title)
+      )
+    )
+    preview_format$pandoc$from <- "gfm"
     rmarkdown::render(
       output_file,
-      output_format = rmarkdown::html_document(
-        highlight = if (gray_preview) "monochrome" else "default",
-        css = if (gray_preview) system_file("css", "html-preview.css"),
-        pandoc_args = c(
-          "--metadata", sprintf('title="%s"', metadata$title)
-        )
-      ),
+      output_format = preview_format,
       output_file = xfun::with_ext(output_file, "html")
     )
 
