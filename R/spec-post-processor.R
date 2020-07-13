351c72a6055ae9b42c45fbd2c19f019d1c62355b
atusy <30277794+atusy@users.noreply.github.com>
2020-07-13 21:21:28 +0900

    fix math rendering issues

--- R/spec-post-processor.R
+++ R/spec-post-processor.R
@@ -6,14 +6,12 @@ spec_post_processor <- function(gray_preview) {
       highlight = if (gray_preview) "monochrome" else "default",
       css = if (gray_preview) system_file("css", "html-preview.css"),
       pandoc_args = c(
-        "--metadata", sprintf('title="%s"', metadata$title)
-      ),
-      includes = list(in_header = system.file(package = 'gihyodown', 'html', 'katex.html')),
-      math = NULL
+        "--metadata", sprintf('title="%s"', metadata$title), "--mathjax"
+      )
     )
 
     rmarkdown::render(
-      output_file,
+      input_file,
       output_format = preview_format,
       output_file = xfun::with_ext(output_file, "html")
     )
