4e18df0bc58eddde183512f2ac3e38e18729d836
atusy <30277794+atusy@users.noreply.github.com>
2020-12-12 23:16:26 +0900

    add toc on preview

--- R/spec-post-processor.R
+++ R/spec-post-processor.R
@@ -5,6 +5,7 @@ spec_post_processor <- function(gray_preview) {
     preview_format <- rmarkdown::html_document(
       highlight = if (gray_preview) "monochrome" else "default",
       css = if (gray_preview) system_file("css", "html-preview.css"),
+      toc = TRUE, toc_float = TRUE,
       pandoc_args = c(
         "--metadata", sprintf('title="%s"', metadata$title), "--mathjax",
         "--lua-filter", system_file("lua", "crossref.lua")
