a03d0cc0eecbd8c29cbaf62681010659a2fe4f38
atusy <30277794+atusy@users.noreply.github.com>
2022-10-26 00:47:32 +0900

    add missing R/style-gfm.R

---
+++ R/style-gfm.R
@@ -0,0 +1,34 @@
+#' Style R code blocks in gihyo-flavored markdown file
+#'
+#' @param path Paths to markdown files
+#' @param ... Arguments passed to `styler::style_file()`
+#'
+#' @export
+style_gfm <- function(path, ...) {
+  lapply(path, style_gfm_one)
+  return(path)
+}
+
+style_pandoc <- function(input, output, ...) {
+  rmarkdown::pandoc_convert(
+    input, to = "gfm", from = "gfm", output = output,
+    options = c(
+      ...,
+      "--wrap=none"
+    )
+  )
+}
+
+style_gfm_one <- function(path, ...) {
+  md <- tempfile(fileext = ".Rmd")
+  style_pandoc(
+    path, md,
+    "--lua-filter", system_file("lua", "chunkify.lua")
+  )
+  styler::style_file(md)
+  style_pandoc(
+    md, md,
+    "--lua-filter", system_file("lua", "unchunkify.lua"),
+  )
+  file.copy(md, path, overwrite = TRUE)
+}
